00f672dfe856b242f556c706b39647a1268d34df
Brayan <brayanortegabco@gmail.com>
2024-11-28 02:21:20 -0500

    solucion

--- worker.js
+++ worker.js
@@ -1,33 +1,71 @@
-self.addEventListener('install', function(event){
-    event.waitUntil(
-     caches.open('first-app')
-     .then(function(cache) {
-         cache.addAll([
-             './',
-              './index.html',
-              './pages/cotizacion.html',
-              './css/styleCotizacion.css',
-              './css/stylePrincipal.css',
-              './dist/serviceWork.js',
-              './dist/cotizacion.js',
-              './pages/cotizacion.html',
-              './manifest.json',
-              './imagenes/Logo FARTTEK.png',
-              './imagenes/brayan.jpeg',
-              './imagenes/Daniel Laguna.jpeg',
-              './imagenes/portada.jpg',
-              './imagenes',
-         ])
-     })
-    );
-    return self.clients.claim();
- });
- 
- self.addEventListener('fetch', function(event){
-     event.respondWith(
-         caches.match(event.request)
-         .then(function(res){
-             return res;
-         })
-     );
- });
+const CACHE_NAME = 'CacheFar';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/styleCotizacion.css',
+  '/css/stylePrincipal.css',
+  '/worker.js',
+  '/manifest.json',
+  '/pages/cotizacion.html',
+  '/dist/cotizacion.js',
+  '/imagenes/Bog Shoes.png',
+  '/imagenes/brayan.jpeg',
+  '/imagenes/Daniel Laguna.jpeg',
+  '/imagenes/facebook.png',
+  '/imagenes/instagram.png',
+  '/imagenes/linkedin.png',
+  '/imagenes/Logo FARTTEK.png',
+  '/imagenes/maps.png',
+  '/imagenes/marketing.png',
+  '/imagenes/portada.jpg',
+  '/imagenes/Portada2.jpg',
+  '/imagenes/puntaDAnca.png',
+  '/imagenes/whatsapp.png',
+  '/imagenes/logo app1.png',
+  '/imagenes/logo app3.png',
+  '/imagenes/pan1.png',
+  '/imagenes/pan2.png',
+];
+
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache).catch((err) => {
+        console.error('Error cacheando archivos:', err);
+      });
+    })
+  );
+});
+
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Eliminando cache antiguo:', cache);
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+});
+
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).catch(() => {
+          if (event.request.destination === 'document') {
+            return caches.match('/index.html'); 
+          }
+        })
+      );
+    })
+  );
+});
