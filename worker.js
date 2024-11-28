170be0ed770bb8bfaaab616c32c1abb066fa757f
Brayan <brayanortegabco@gmail.com>
2024-11-27 22:44:21 -0500

    Implementacion de Worker Cache para cuando se valla el wifi

---
+++ worker.js
@@ -0,0 +1,27 @@
+self.addEventListener('install', function(event){
+    event.waitUntil(
+     caches.open('first-app')
+     .then(function(cache) {
+         cache.addAll([
+             '/',
+              '/index.html',
+              '/pages/cotizacion.html',
+              '/css/styleCotizacion.css',
+              '/css/stylePrincipal.css',
+              '/dist/serviceWork.js',
+              '/dist/cotizacion.js',
+              '/pages/cotizacion.html',
+         ])
+     })
+    );
+    return self.clients.claim();
+ });
+ 
+ self.addEventListener('fetch', function(event){
+     event.respondWith(
+         caches.match(event.request)
+         .then(function(res){
+             return res;
+         })
+     );
+ });
